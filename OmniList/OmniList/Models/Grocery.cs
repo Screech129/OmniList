73b29c19c5107fad030c668f83e78e74dd4bbbf3
david.silver <dsilver2989@gmail.com>
2017-02-09 13:17:56 -0600

    Initial Push (with grocery model)

---
+++ OmniList/OmniList/Models/Grocery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace OmniList.Models
+{
+    public class Grocery
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public Guid UserId { get; set; }
+        public bool Removed { get; set; }
+
+
+        [Version]
+        public string AzureVersion { get; set; }
+
+        [CreatedAt]
+        public DateTimeOffset? AzureCreatedDate { get; set; }
+
+        [UpdatedAt]
+        public DateTimeOffset? AzureUpdatedDate { get; set; }
+
+    }
+}
